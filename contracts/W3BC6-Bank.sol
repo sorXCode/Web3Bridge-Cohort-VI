6ee73d6d5fbcaa15622120ae5dae094ba713903c
sorxcode <sorxcode@gmail.com>
2022-02-04 19:00:25 +0100

    [task] added bank contract

---
+++ contracts/W3BC6-Bank.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+
+
+contract Bank {
+    /* Features: 
+        - Deposit => user can pay into their account
+        - withdraw => user can withdraw from their account
+        - get_users_balance => user can view their account balance
+        - get_bank_balance => the contract owner can view the total amount in the bank 
+        - transfer => user can transfer funds to another account
+        ------------------------------------------------------------------------------------------
+        improvements:
+        - use fallback/receive method
+        - separate contracts: one for bank, and the other for user
+        - apply DRY(Don't Repeat Yourself) to withdraw and transfer_to_address functions
+        - check against 0x0000000000000000000000000000000000000000 address for transactions
+    */
+
+}
