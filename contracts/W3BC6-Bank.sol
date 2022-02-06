2efb8dcdf0057ebe90d70a6f581698326da7eb23
Olajumoke <rukkylaw.tl@gmail.com>
2022-02-06 19:55:57 +0100

    Fixes

--- contracts/W3BC6-Bank.sol
+++ contracts/W3BC6-Bank.sol
@@ -17,18 +17,20 @@ contract Bank {
         - check against 0x0000000000000000000000000000000000000000 address for transactions
     */
      address immutable private owner;
-       constructor (address _owner) {
-         owner = _owner;
+       constructor () {
+         owner = msg.sender;
      }
 
-    mapping(address => uint) User_Balance;
+    mapping(address => uint) user_balance;
+    uint private bank_balance; 
 
 
     function transfer (uint _amount, address _to) external returns (bytes memory){
-        require (User_Balance[msg.sender] >= _amount, 'Insufficent funds');
-        User_Balance[msg.sender] -= _amount;
-         (bool sent, bytes memory data) = _to.call{value: _amount}('');
-         require(sent, "transacation failed");
-         return data;
+        require (user_balance[msg.sender] >= _amount, 'Insufficent funds');
+        user_balance[msg.sender] -= _amount;
+        bank_balance -= _amount;
+        (bool sent, bytes memory data) = _to.call{value: _amount}('');
+        require(sent, "transacation failed");
+        return data;
     }
 }
